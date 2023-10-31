88542a7df01f6bdb1a701709d36b820313492b72
Abhishek Pratap Singh <abhishekprataps656@gmail.com>
2023-10-31 13:15:29 +0530

    Added separate module ofr mondoDB

---
+++ models/contact.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) =>{
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Entry', userSchema)
