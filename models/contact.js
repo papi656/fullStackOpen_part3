6f7db1be67e03b5ca8e7989ba2b489516d6bb1fd
Abhishek Pratap Singh <abhishekprataps656@gmail.com>
2023-11-03 08:14:24 +0530

    Changes due to linter

--- models/contact.js
+++ models/contact.js
@@ -1,42 +1,40 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 // const url = process.env.MONGODB_URI
-const url = process.env['MONGODB_URI']
+const url = process.env.MONGODB_URI;
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) =>{
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const userSchema = new mongoose.Schema({
-    name:{ 
-        type: String,
-        minLength: 3,
-        required: [true, 'User name required']
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'User name required'],
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => /^\d{2,3}-\d{6,}$/.test(v),
+      message: (props) => `${props.value} is not a valid phone number`,
     },
-    number: {
-        type: String,
-        validate: {
-            validator: v => {
-                return /^\d{2,3}-\d{6,}$/.test(v)
-            },
-            message: props => `${props.value} is not a valid phone number`
-        },
-        required: [true, 'User phone number required']
-    }
-})
+    required: [true, 'User phone number required'],
+  },
+});
 
 userSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Entry', userSchema)
+module.exports = mongoose.model('Entry', userSchema);
